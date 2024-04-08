9fc87f833bc2eb91562846cef9f4e4d9786c601c
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-04-07 23:59:36 -0400

    added footer

--- src/module/footer/footer.js
+++ src/module/footer/footer.js
@@ -1,5 +1,69 @@
 import React from "react";
+import Image from "next/image";
+
+import logo from "../../../public/image/logoBrown.png";
+import styles from "./footer.module.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.section1}>
+        {/* <div> */}
+        <Image src={logo} alt="logo" className={styles.logo} />
+        <div className={styles.aboutText}>
+          Welcome to <span className={styles.brown}>Brown Man Printing</span>{" "}
+          Shop, where convenience meets quality just steps away from the
+          University of Windsor! Whether you need printing, photocopying,
+          scanning, or professional CV services, we've got you covered at
+          unbeatable prices.
+        </div>
+        {/* </div> */}
+      </div>
+      <div className={styles.section2}>
+        <div className={styles.title}>Hours</div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Saturday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Sunday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Monday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Tuesday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Wednesday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Thursday</div>
+          <div className={styles.time}>7a.m. - 11p.m.</div>
+        </div>
+        <div className={styles.hoursSection}>
+          <div className={styles.day}>Friday</div>
+          <div className={styles.time}>7a.m. - 12p.m. | 3p.m. - 11p.m.</div>
+        </div>
+      </div>
+      <div className={styles.section3}>
+        <div className={styles.title}>Contact Us</div>
+        <div className={styles.text}>
+          <span className={styles.bold}>Phone:</span> +1(647)808-3578
+        </div>
+        <div className={styles.text}>
+          <span className={styles.bold}>Email:</span>{" "}
+          ayonchowdhury1997@gmail.com
+        </div>
+        <div className={styles.text}>
+          <span className={styles.bold}>Address:</span> 430 Rosedale Ave,
+          Windsor, ON
+        </div>
+      </div>
+    </footer>
+  );
 }
