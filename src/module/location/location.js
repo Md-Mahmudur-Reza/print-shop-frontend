9885b8a1fcae0f16ba43182bb1ba4c5be17940e9
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-04-23 13:48:58 -0400

    removed direction part from location

--- src/module/location/location.js
+++ src/module/location/location.js
@@ -6,13 +6,12 @@ import styles from "./location.module.css";
 
 export default function Location() {
   const location = { lat: 42.30461120605469, lng: -83.0713119506836 };
-  const destination = { lat: 42.30461120605469, lng: -83.0713119506836 };
 
   return (
     <div className={styles.locationSection} id="location">
       <h1 className={styles.locationTitle}>Location</h1>
       <div className={styles.mapSection}>
-        <MapContainer location={location} destination={destination} />
+        <MapContainer location={location} />
       </div>
     </div>
   );
--- src/module/location/mapContainer.js
+++ src/module/location/mapContainer.js
@@ -7,9 +7,9 @@ import {
   DirectionsRenderer,
 } from "@react-google-maps/api";
 
-const MapContainer = ({ location, destination }) => {
-  const [directions, setDirections] = useState(null);
-  const [directionsUrl, setDirectionsUrl] = useState("");
+const MapContainer = ({ location }) => {
+  // const [directions, setDirections] = useState(null);
+  // const [directionsUrl, setDirectionsUrl] = useState("");
 
   const mapStyles = {
     height: "400px",
@@ -18,44 +18,17 @@ const MapContainer = ({ location, destination }) => {
     border: "solid  chocolate 2px",
   };
 
-  useEffect(() => {
-    const loadDirections = () => {
-      if (!window.google || !window.google.maps) return;
-
-      const directionsService = new window.google.maps.DirectionsService();
-
-      directionsService.route(
-        {
-          origin: location,
-          destination: destination,
-          travelMode: window.google.maps.TravelMode.DRIVING,
-        },
-        (result, status) => {
-          if (status === window.google.maps.DirectionsStatus.OK) {
-            setDirections(result);
-            const directionsUrl = `https://www.google.com/maps/dir/?api=1&origin=${location.lat},${location.lng}&destination=${destination.lat},${destination.lng}`;
-            setDirectionsUrl(directionsUrl);
-          } else {
-            console.error(`error fetching directions ${result}`);
-          }
-        }
-      );
-    };
-
-    loadDirections();
-  }, [location, destination]);
-
   return (
     <div>
       <LoadScript googleMapsApiKey="AIzaSyCLRlZFHfYyUU5Ka6kY1bPsnO-tS8ttEY8">
         <GoogleMap mapContainerStyle={mapStyles} zoom={10} center={location}>
           <Marker position={location} />
-          {directions && <DirectionsRenderer directions={directions} />}
+          {/* {directions && <DirectionsRenderer directions={directions} />}
           {directionsUrl && (
             <a href={directionsUrl} target="_blank" rel="noopener noreferrer">
               Get Directions
             </a>
-          )}
+          )} */}
         </GoogleMap>
       </LoadScript>
     </div>
