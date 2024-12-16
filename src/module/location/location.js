84b41783b8fe000a2e94126fdf10dd6c6df4d277
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-12-16 17:56:14 -0500

    commented map

--- src/module/location/location.js
+++ src/module/location/location.js
@@ -1,18 +1,18 @@
-"use client";
-import React from "react";
+// "use client";
+// import React from "react";
 
-import MapContainer from "./mapContainer";
-import styles from "./location.module.css";
+// import MapContainer from "./mapContainer";
+// import styles from "./location.module.css";
 
-export default function Location() {
-  const location = { lat: 42.30461120605469, lng: -83.0713119506836 };
+// export default function Location() {
+//   const location = { lat: 42.30461120605469, lng: -83.0713119506836 };
 
-  return (
-    <div className={styles.locationSection} id="location">
-      <h1 className={styles.locationTitle}>Location</h1>
-      <div className={styles.mapSection}>
-        <MapContainer location={location} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.locationSection} id="location">
+//       <h1 className={styles.locationTitle}>Location</h1>
+//       <div className={styles.mapSection}>
+//         <MapContainer location={location} />
+//       </div>
+//     </div>
+//   );
+// }
